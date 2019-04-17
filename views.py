410d2aace81212fc16912817502664d9885f0d5d
muhammad-usman147 <35249668+muhammad-usman147@users.noreply.github.com>
2019-04-17 22:50:32 +0500

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Stocks_Details
-# Create your views here.
-def show(request):
-    sd=Stocks_Details.objects.all()
-    return render(request,'details.html',{'sd_d':sd})
